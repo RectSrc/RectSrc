a264caa886ba8f5f3ef4cb0e27ffc72a84af3f47
Khhs167 <34920681+Khhs167@users.noreply.github.com>
2021-05-14 17:56:22 +0200

    Updated recc interfaces

--- rsp/Core/Ext/ReCTInterface.cs
+++ rsp/Core/Ext/ReCTInterface.cs
@@ -8,5 +8,49 @@ namespace RectSrc
     public static class RectSrc
     {
         public static Core.Game.Entities.Entity GetEntity(string name) => Core.Game.GameManager.level.GetEntity(name);
+        public static void DrawCube(Vector3 pos, Vector3 size, Color color) => Raylib_cs.Raylib.DrawCubeV((System.Numerics.Vector3)pos.systemized,(System.Numerics.Vector3)size.systemized, (Raylib_cs.Color)color.ToRaylib());
+
+
+
+
+        //Class definitions
+        public class Vector3
+        {
+            private Core.Math.Vector3 vector3;
+            public float x => vector3.x;
+            public float y => vector3.y;
+            public float z => vector3.z;
+            public float magnitude() => vector3.magnitude;
+            public Vector3 normalized => FromInternal(vector3.normalized);
+            public Vector3(float x, float y, float z)
+            {
+                vector3 = new Core.Math.Vector3(x, y, z);
+            }
+            private static Vector3 FromInternal(Core.Math.Vector3 vector3)
+            {
+                return new Vector3(vector3.x, vector3.y, vector3.z);
+            }
+
+            public object systemized => vector3.systemized;
+        }
+
+        public class Color
+        {
+            private Raylib_cs.Color color;
+            public byte r => color.r;
+            public byte g => color.g;
+            public byte b => color.b;
+            public byte a => color.a;
+
+            public Color(int r, int g, int b, int a)
+            {
+                color = new Raylib_cs.Color(r, g, b, a);
+            }
+
+            public object ToRaylib()
+            {
+                return (object)color;
+            }
+        }
     }
 }
